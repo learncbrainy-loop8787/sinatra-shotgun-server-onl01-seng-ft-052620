6a267083296bae352e4f22e7900a16c9d9dd4d27
korey ko <koreykendrick1@gmail.com>
2020-07-04 02:58:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! welcome to the future of this shit!!! keep my team on ice, live longer than christ. "
+    " welcome to the future of this shit!!! keep my team on ice, live longer than christ. "
   end
 
 end
